0435750f12e8b83e3231e5e0d911f056cd6329c7
Roman_18 <roman.ak18@gmail.com>
2017-10-09 22:21:44 +0300

    first commit

---
+++ src/kyiv/magic/Dota2.java
@@ -0,0 +1,18 @@
+package kyiv.magic;
+
+import ru.dota2.BootsOfTravel;
+import ua.com.zues.Hero;
+
+public class Dota2 {
+
+	public static void main(String[] args) {
+		
+		BootsOfTravel boots = new BootsOfTravel();
+		Hero hero = new Hero();
+		
+		System.out.println(boots.toString()+" "+hero.toString());
+		
+
+	}
+
+}
---
+++ src/ru/dota2/BootsOfTravel.java
@@ -0,0 +1,23 @@
+package ru.dota2;
+
+public class BootsOfTravel {
+	
+	private String  boots= "Boots of travel";
+
+	public BootsOfTravel() {
+		
+		toString();
+		
+	}
+
+	@Override
+	public String toString() {
+		return "BootsOfTravel [boots=" + boots + "]";
+	}
+	
+	
+	
+	
+	
+
+}
