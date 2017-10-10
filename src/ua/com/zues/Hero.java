f28f36f3b808ef342f9465275600c0943de34251
Roman_18 <roman.ak18@gmail.com>
2017-10-10 19:53:59 +0300

    v2

---
+++ src/ua/com/zues/Hero.java
@@ -0,0 +1,19 @@
+package ua.com.zues;
+
+public class Hero {
+
+	private String name = "Zues";
+	
+	public Hero() {
+		toString();
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Hero [name=" + name + "]";
+	}
+	
+
+}
